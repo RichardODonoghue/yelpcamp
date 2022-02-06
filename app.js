a0bd3a47809d004edbcff95c1cea340d6129faed
Richard O'Donoghue <gymfreak7855@gmail.com>
2022-02-07 01:18:46 +1300

    TRYING TO FIX issues

--- app.js
+++ app.js
@@ -173,6 +173,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`);
 })
